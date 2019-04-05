26e2fe33c885c6a68a0559c26a26b41df6432a7b
Lewis Marshall <lewis@technoplusit.co.uk>
2019-04-05 12:34:22 +0100

    daemonset and support envs

--- cmd/mpodr.go
+++ cmd/mpodr.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/appvia/metal-pod-reaper/pkg/mpodr"
 	"github.com/appvia/metal-pod-reaper/pkg/version"
@@ -21,10 +22,10 @@ func Execute() {
 	var namespace string
 	var hostIP string
 
-	flag.BoolVar(&dryRun, "dry-run", true, "only report on potential changes")
+	flag.BoolVar(&dryRun, "dry-run", true, "only report on potential changes (env - DRY_RUN)")
 	flag.BoolVar(&reap, "no-reap", true, "do not run the reap facility")
-	flag.StringVar(&namespace, "namespace", "", "namespace for the master leaselock object")
-	flag.StringVar(&hostIP, "host-ip", "", "specify the host ip")
+	flag.StringVar(&namespace, "namespace", "", "namespace for the master leaselock object (env - NAMESPACE)")
+	flag.StringVar(&hostIP, "host-ip", "", "specify the host ip (env - HOST_IP)")
 	flag.BoolVar(&ver, "version", false, "display the version")
 	flag.Parse()
 
@@ -43,6 +44,14 @@ func Execute() {
 		hostIP = os.Getenv("HOST_IP")
 		if hostIP == "" {
 			klog.Fatal("Expecting HOST_IP to be set")
+		}
+	}
+	dryRunStr := os.Getenv("DRY_RUN")
+	if len(dryRunStr) > 0 {
+		if b, err := strconv.ParseBool(dryRunStr); err != nil {
+			klog.Fatalf("Expecting bool in DRY_RUN not %s", dryRunStr)
+		} else {
+			dryRun = b
 		}
 	}
 	if err := mpodr.Run(reap, dryRun, namespace, hostIP); err != nil {
